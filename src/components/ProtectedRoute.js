807cf157e65e15c81001b12bdb4db5e3e951178f
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2022-09-17 09:42:38 -0500

    Modify ProtectedRoute to allow persistance login

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,14 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 const ProtectedRoute = ({ children }) => {
-  const { userNickname } = useContext(AppContext);
+  const { userNickname, setUserData } = useContext(AppContext);
+  const userFromLocalStorage = window.localStorage.getItem('userNickname');
+
+  useEffect(() => {
+    if (userFromLocalStorage && !userNickname) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `https://api.github.com/users/${userFromLocalStorage}`
+          );
+
+          if (response.ok) {
+            const userData = await response.json();
+            setUserData(userData);
+            console.log(userData);
+            // window.localStorage.setItem('userNickname', userData.login);
+          }
+        } catch (error) {
+          console.error(error.message);
+        }
+      };
+
+      fetchData();
+    }
+  }, []);
 
   if (userNickname) {
     return children;
   }
 
-  return <Navigate to='/login' />;
+  if (!userFromLocalStorage && !userNickname) {
+    return <Navigate to='/login' />;
+  }
+
+  return null;
 };
 
 export default ProtectedRoute;
