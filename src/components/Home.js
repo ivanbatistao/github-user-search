951aab0710f2d3cf8fe4e3f658b40f1f07e5ab79
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2022-09-17 09:06:53 -0500

    Add context to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Home = () => {
+  const state = useContext(AppContext);
+  console.log(state);
   return <div>Home</div>;
 };
 
