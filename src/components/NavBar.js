aa3be2c3972f607ea968fd3152d9fb849583d079
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2022-09-17 11:56:27 -0500

    Add Links, logout feature, user name and avatar to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,78 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link, redirect, useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 
 const NavBar = ({ isUserInfoPresent }) => {
+  const { userData, setUserData } = useContext(AppContext);
+  const [showLogout, setShowLogout] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleMouseOver = () => {
+    setShowLogout(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowLogout(false);
+  };
+
+  const handleClick = () => {
+    window.localStorage.clear();
+    setUserData({});
+    navigate('/login');
+  };
+
   return (
-    <nav>
-      <ul>
-        <li></li>
+    <header>
+      <div>
+        <figure>
+          <img
+            src='https://www.securityopenlab.it/immagini/2020/06/octocat-github-1.jpg'
+            width={200}
+            height={120}
+            alt='logo'
+          ></img>
+        </figure>
         {isUserInfoPresent && (
-          <>
-            <li></li>
-            <li></li>
-            <li></li>
-          </>
+          <div>
+            <ul>
+              <li>
+                <Link to='/info'>User Info</Link>
+              </li>
+              <li>
+                <Link to='/repos'>Repos</Link>
+              </li>
+              <li
+                style={{ border: '1px red solid' }}
+                onMouseOver={handleMouseOver}
+                onMouseLeave={handleMouseLeave}
+              >
+                ...
+              </li>
+              {showLogout && (
+                <li
+                  style={{ border: '1px blue solid' }}
+                  onMouseOver={handleMouseOver}
+                  onMouseLeave={handleMouseLeave}
+                  onClick={handleClick}
+                >
+                  Logout
+                </li>
+              )}
+            </ul>
+            <div>
+              <span>{userData.name}</span>
+              <img
+                src={userData.avatar_url}
+                width={50}
+                height={50}
+                alt='logo'
+              ></img>
+            </div>
+          </div>
         )}
-      </ul>
-    </nav>
+      </div>
+    </header>
   );
 };
 
