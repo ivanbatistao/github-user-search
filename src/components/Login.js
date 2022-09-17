427e49ab4cdfeb3fe6e31764ead7de044f8652b4
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2022-09-17 09:38:36 -0500

    Add Login feature - allow users to type userNickname and redirect them to home or show an error

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 
 const Login = () => {
-  return <div>Login</div>;
+  const { userNickname, setUserData } = useContext(AppContext);
+  const userFromLocalStorage = window.localStorage.getItem('userNickname');
+
+  const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState('');
+
+  if (userNickname || userFromLocalStorage) {
+    return <Navigate to='/' />;
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!inputValue) return;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${inputValue}`
+        );
+
+        if (response.ok) {
+          const userData = await response.json();
+          setUserData(userData);
+          window.localStorage.setItem('userNickname', userData.login);
+        } else {
+          throw new Error('User Not Found');
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={inputValue} onChange={handleChange} />
+      <button type='submit'>Login</button>
+      {error && <span>{error}</span>}
+    </form>
+  );
 };
 
 export default Login;
