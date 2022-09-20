8cff0b4731b462ed38e5d5053830dc8308c2448c
Ivan E. Batista-Ochoa <batistaochoaivan@gmail.com>
2022-09-20 17:47:42 -0500

    Rename Home component: from Home to  Welcome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
-import Home from './components/Home';
+import Welcome from './components/Welcome';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import NotFound from './components/NotFound';
@@ -23,7 +23,7 @@ function App() {
               </ProtectedRoute>
             }
           >
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Welcome />} />
             <Route path='/info' element={<UserInfo />} />
             <Route path='/repos' element={<div>Repos</div>} />
           </Route>
